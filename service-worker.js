e8108154a35c3e538af25885bd368425ed8e131a
ToeGGeli-Cup <bzemann@gmail.com>
2025-03-02 14:18:28 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+
+// Service Worker für automatische Updates
+self.addEventListener("install", (event) => {
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    return caches.delete(cache);
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        fetch(event.request).catch(() => caches.match(event.request))
+    );
+});
